0fbe92927c4b42702e1966394cf978c5dd723937
hazartamer <155576161+hazartamer@users.noreply.github.com>
2024-04-16 12:41:39 +0300

    Add files via upload

---
+++ src/main/java/com/example/springbootodevister/services/abstracts/ProductService.java
@@ -0,0 +1,16 @@
+package com.example.springbootodevister.services.abstracts;
+
+import com.example.springbootodevister.services.dtos.request.AddProductRequest;
+import com.example.springbootodevister.services.dtos.response.AddProductResponse;
+import com.example.springbootodevister.services.dtos.response.ListProductResponse;
+
+import java.util.List;
+
+public interface ProductService {
+     AddProductResponse add(AddProductRequest request);
+     List<ListProductResponse> getAll();
+     List<ListProductResponse> searchByName(String name);
+
+     List<ListProductResponse> searchByUnitPrice(int minPrice, int maxPrice);
+
+}
